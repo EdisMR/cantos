50692f1efc554dc77a2b028ff9bd72d038a05595
Edis MR <edisanthony@gmail.com>
2025-06-07 17:11:22 -0600

    style corrections and new song

--- js/index.js
+++ js/index.js
@@ -50,6 +50,7 @@ const cantos = [
 	{ id: "12", name: "MNM RCCES Mex 2018 - Hay poder", categories: ["1E"], fileUrl: 'c/0012.html' },
 	{ id: "13", name: "Marcos Witt - Quiero levantar mis manos", categories: ["1E"], fileUrl: 'c/0013.html' },
 	{ id: "14", name: "Jesed - Que hermoso es", categories: ["6"], fileUrl: 'c/0014.html' },
+	{ id: "15", name: "Tuya es mi vida", categories: ["6"], fileUrl: 'c/0015.html' },
 ]
 /* { id: "", name: "", categories: [""], fileUrl: 'c/00.html' }, */
 
